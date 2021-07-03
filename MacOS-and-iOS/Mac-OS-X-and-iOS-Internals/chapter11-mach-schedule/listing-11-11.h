fc443ac2f71775163b12ed3bfa9a8fc2f296522e
mikechen66 <875661606@qq.com>
2021-07-03 21:06:01 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter11-mach-schedule directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter11-mach-schedule/listing-11-11.h
+++
@@ -1,5 +0,0 @@
-// Listing 11-11 Setting ASTs in osfmk/kern/ast.h
-
-#define thread_ast_set(act, reason) (hw_atomic_or_noret(&(act)->ast, (reason)))
-#define thread_ast_clear(act, reason) (hw_atomic_and_noret(&(act)->ast, ∼(reason)))
-#define thread_ast_clear_all(act) (hw_atomic_and_noret(&(act)->ast, AST_NONE))
--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter11-mach-schedule/listing-11-16.c
+++
@@ -1,8 +0,0 @@
-// Listing 11-16: The now deprecated Hz hardware interval, in bsd/kern/kern_clock.c
-
-/*
- * The hz hardware interval timer.
- */
-
-int hz = 100;                /* GET RID OF THIS !!! */
-int tick = (1000000 / 100);  /* GET RID OF THIS !!! */
