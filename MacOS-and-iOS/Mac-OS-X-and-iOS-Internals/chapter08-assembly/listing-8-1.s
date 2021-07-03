212f74440e3fa20d243fa763200aae41ac7700d1
mikechen66 <875661606@qq.com>
2021-07-03 21:21:24 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter08-assembly directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter08-assembly/listing-8-1.s
+++
@@ -1,9 +0,0 @@
-// Listing 8-1: osfmk/x86_64/start.s 
-
-Entry(real_mode_bootstrap_base)
-    cli
-    LGDT(EXT(protected_mode_gdtr))
-    /* set the PE bit of CR0 */
-    mov     %cr0, %eax      ; can't operate on CRs directly
-    inc %eax                ; add 1 toggles on the least significant bit
-    mov     %eax, %cr0      ; update CR0
