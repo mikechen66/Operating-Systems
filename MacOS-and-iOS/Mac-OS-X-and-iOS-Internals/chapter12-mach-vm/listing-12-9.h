a652f39a53de54d73e83908ac7ceadddef505777
mikechen66 <875661606@qq.com>
2021-07-03 20:54:31 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter12-mach-vm directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter12-mach-vm/listing-12-9.h
+++
@@ -1,5 +0,0 @@
-// Listing 12-9: Some of the kalloc functions in osfmk/kern/kalloc.h
-
-extern void *kalloc(vm_size_t   size);
-extern void *kalloc_noblock(vm_size_t   size);
-extern void  kfree(void *data, vm_size_t size);
