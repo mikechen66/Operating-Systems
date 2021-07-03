4f5a534905031517a8e5dadcb22b10e309020305
mikechen66 <875661606@qq.com>
2021-07-03 19:49:28 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter15-file-system directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter15-file-system/listing-15-6.h
+++
@@ -1,6 +0,0 @@
-// Listing 15-6: vfs_fsadd and vfs_fsremove, as defined in bsd/sys/mount.h
-
-// Add a File system to VFS — provide vfs_fsentry, get vfs_table_t  handle
-int vfs_fsadd(_in_ struct vfs_fsentry *, _out_ vfstable_t *);
-// Remove a File system from VFS, given the vfstable_t handle
-int vfs_fsremove(_in_ vfstable_t);
