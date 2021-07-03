fb2c0abd31cd6acb6e207290c50fba6a2957d52d
mikechen66 <875661606@qq.com>
2021-07-03 20:17:57 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter15-file-system directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter15-file-system/listing-15-17.c
+++
@@ -1,6 +0,0 @@
-// Listing 15-17: fo_read from bsd/kern/kern_descript.c
-
-int fo_read(struct fileproc *fp, struct uio *uio, int flags, vfs_context_t ctx) {
-    // simple pass through. Remember that by f_ops we mean f_fglob->f_ops
-    return ((*fp->f_ops->fo_read)(fp, uio, flags, ctx));
-}
