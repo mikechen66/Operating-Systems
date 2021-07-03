3a5d063ba2a57263f9fb7c3e919f8e1a46e2ec0e
mikechen66 <875661606@qq.com>
2021-07-03 21:13:11 +0800

    Delete MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter09-kernel-boot directory

--- MacOS-and-iOS/Mac-OS-X-and-iOS-Internals/chapter09-kernel-boot/listing-9-8.c
+++
@@ -1,5 +0,0 @@
-// Listing 9-8: Testing a panic image (OS X only)
-
-size_t len = 0; 
-int name[3] = { CTL_KERN, KERN_PANICINFO, KERN_PANICINFO_IMAGE + 2 }; 
-sysctl(name, 3, NULL, (void *)&len, NULL, 0);
