030bc0adbc0b21da8554406f062c8a306aae8185
mikechen66 <875661606@qq.com>
2021-07-05 22:44:55 +0800

    Add files via upload

---
+++ Modern-Operating-System/chapter06-deadlocks/figure-06-01.c
@@ -0,0 +1,29 @@
+
+// Figure 6-1. Using a semaphore to protect resources. 
+
+
+// (a) One resource. 
+
+typedef int semaphore; 
+semaphore resource 1;
+
+void process_A(void) { 
+    down(&resource_1);
+    use_resource_1(); 
+    up(&resource_1);
+}
+
+
+// (b) Two resources.
+
+typedef int semaphore;
+semaphore resource_1; 
+semaphore resource_2;
+
+void process_A(void) { 
+    down(&resource_1); 
+    down(&resource_2);
+    use_both_resources(); 
+    up(&resource_2); 
+    up(&resource_1);
+}
