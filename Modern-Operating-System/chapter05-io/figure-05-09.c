1ec6583c6c1efb373ee23834b649c56c40cf5fb7
mikechen66 <875661606@qq.com>
2021-07-05 22:41:12 +0800

    Add files via upload

---
+++ Modern-Operating-System/chapter05-io/figure-05-09.c
@@ -0,0 +1,25 @@
+
+// Figure 5-9. Writing a string to the printer using interrupt-driven I/O. 
+
+
+// (a) Code executed at the time the print system call is made. (b) Interrupt service procedure for the printer.
+
+copy_from_user(buffer, p, count); 
+enable_interrupts();  
+while (*printer_status_reg != READY);
+*printer_data_register() = p[0];
+scheduler();
+
+
+// (b) Interrupt service procedure for the printer.
+
+if (count == 0) { 
+    unlock_user();
+} else{
+    *printer_data_register = p[i]; 
+    count = count − 1;
+    i = i + 1; 
+}
+
+acknowledge_interrupt(); 
+return_from_interrupt();
