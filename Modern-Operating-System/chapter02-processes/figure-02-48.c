4abfdcc6a16200597d24f0e38469531efb3ee9c3
mikechen66 <875661606@qq.com>
2021-07-05 22:28:52 +0800

    Add files via upload

--- Modern-Operating-System/chapter02-processes/figure-02-48.c
+++ Modern-Operating-System/chapter02-processes/figure-02-48.c
@@ -1,37 +1,37 @@
 
 // Figure 2-48. A solution to the readers and writers problem
 
-typedef int semaphore; 
-semaphore mutex = 1; 
-semaphore db = 1;
-int rc = 0;
+typedef int semaphore;       /* use your imagination */
+semaphore mutex = 1;         /* controls access to rc */
+semaphore db = 1;            /* controls access to the database */
+int rc = 0;                  /* # of processes reading or wanting to */
 
 
 void reader(void) {
     while (TRUE) { 
-        down(&mutex);
-        rc = rc + 1;
-        if (rc == 1) 
+        down(&mutex);        /* get exclusive access to rc */
+        rc = rc + 1;         /*one reader more now */
+        if (rc == 1)         /* if this is the first reader ... */
             down(&db); 
 
-        up(&mutex);
-        read_data_base(); 
-        down(&mutex);
-        rc = rc − 1;
+        up(&mutex);          /* release exclusive access to rc */
+        read_data_base();    /* access the data */
+        down(&mutex);        /* get exclusive access to rc */
+        rc = rc − 1;         /*one reader fewer now */
 
-        if (rc == 0) 
+        if (rc == 0)         /* if this is the last reader ... */
             up(&db); 
         
-        up(&mutex);
-        use_data_read()
+        up(&mutex);          /* release exclusive access to rc */
+        use_data_read()      /* noncritical region */
     }
 }
 
 void writer(void) {
     while (TRUE) { 
-        think_up_data();
-        down(&db);
-        write_data_base();
-        up(&db);
+        think_up_data();     /* noncritical region */
+        down(&db);           /* get exclusive access */
+        write_data_base();   /* update the data */
+        up(&db);             /* release exclusive access */
     }
 }
