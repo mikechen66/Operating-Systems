2454fb9d58c295344d5c92d9e4f1073ef3501d9c
mikechen66 <875661606@qq.com>
2021-07-06 23:07:25 +0800

    Add files via upload

---
+++ LINUX/Programming/Linux-Programming-Interface/source_code_book/procpri/t_sched_getaffinity.c
@@ -0,0 +1,37 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2020.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU General Public License as published by the   *
+* Free Software Foundation, either version 3 or (at your option) any      *
+* later version. This program is distributed without any warranty.  See   *
+* the file COPYING.gpl-v3 for details.                                    *
+\*************************************************************************/
+
+/* Supplementary program for Chapter 35 */
+
+#define _GNU_SOURCE
+#include <sched.h>
+#include "tlpi_hdr.h"
+
+int
+main(int argc, char *argv[])
+{
+    if (argc != 2 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s pid\n", argv[0]);
+
+    pid_t pid = getInt(argv[1], GN_NONNEG, "pid");
+
+    cpu_set_t set;
+    int s = sched_getaffinity(pid, sizeof(cpu_set_t), &set);
+    if (s == -1)
+        errExit("sched_getaffinity");
+
+    printf("CPUs:");
+    for (int cpu = 0; cpu < CPU_SETSIZE; cpu++)
+        if (CPU_ISSET(cpu, &set))
+            printf(" %d", cpu);
+    printf("\n");
+
+    exit(EXIT_SUCCESS);
+}
