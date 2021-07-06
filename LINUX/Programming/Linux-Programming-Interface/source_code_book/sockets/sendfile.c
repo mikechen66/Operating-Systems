d20a3d87ddaad2f2d9f33c4c695f7ff4800c8c8f
mikechen66 <875661606@qq.com>
2021-07-06 23:12:58 +0800

    Add files via upload

---
+++ LINUX/Programming/Linux-Programming-Interface/source_code_book/sockets/sendfile.c
@@ -0,0 +1,67 @@
+/*************************************************************************\
+*                  Copyright (C) Michael Kerrisk, 2020.                   *
+*                                                                         *
+* This program is free software. You may use, modify, and redistribute it *
+* under the terms of the GNU General Public License as published by the   *
+* Free Software Foundation, either version 3 or (at your option) any      *
+* later version. This program is distributed without any warranty.  See   *
+* the file COPYING.gpl-v3 for details.                                    *
+\*************************************************************************/
+
+/* Solution for Exercise 61-3 */
+
+#include "tlpi_hdr.h"
+#define BUF_SIZE 8192
+
+ssize_t
+sendfile(int out_fd, int in_fd, off_t *offset, size_t count)
+{
+    off_t orig;
+
+    if (offset != NULL) {
+
+        /* Save current file offset and set offset to value in '*offset' */
+
+        orig = lseek(in_fd, 0, SEEK_CUR);
+        if (orig == -1)
+            return -1;
+        if (lseek(in_fd, *offset, SEEK_SET) == -1)
+            return -1;
+    }
+
+    size_t totSent = 0;
+
+    while (count > 0) {
+        size_t toRead = min(BUF_SIZE, count);
+
+        char buf[BUF_SIZE];
+        ssize_t numRead = read(in_fd, buf, toRead);
+        if (numRead == -1)
+            return -1;
+        if (numRead == 0)
+            break;                      /* EOF */
+
+        ssize_t numSent = write(out_fd, buf, numRead);
+        if (numSent == -1)
+            return -1;
+        if (numSent == 0)               /* Should never happen */
+            fatal("sendfile: write() transferred 0 bytes");
+
+        count -= numSent;
+        totSent += numSent;
+    }
+
+    if (offset != NULL) {
+
+        /* Return updated file offset in '*offset', and reset the file offset
+           to the value it had when we were called. */
+
+        *offset = lseek(in_fd, 0, SEEK_CUR);
+        if (*offset == -1)
+            return -1;
+        if (lseek(in_fd, orig, SEEK_SET) == -1)
+            return -1;
+    }
+
+    return totSent;
+}
